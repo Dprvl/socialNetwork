47dffae1d32545ec91f292c314cbfd5e6f538bb9
Dprvl <d.prvlv@gmail.com>
2020-10-02 11:30:03 +0300

    active class added

--- first-react-app/src/components/NavBar/NavBar.jsx
+++ first-react-app/src/components/NavBar/NavBar.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import css from './NavBar.module.css';
 
 const NavBar = () => {
   return (
     <nav className={css.nav}>
       <div className={`${css.navItem} ${css.active}`}>
-        <a href='/profile'>Profile</a>
+        <NavLink to='/profile' activeClassName={css.activeLink}>Profile</NavLink>
       </div>
       <div className={css.navItem}>
-        <a href='/dialogs'>Messages</a>
+        <NavLink to='/dialogs' activeClassName={css.activeLink}>Messages</NavLink>
       </div>
       <div className={css.navItem}>
-        <a href='/news'>News</a>
+        <NavLink to='/news' activeClassName={css.activeLink}>News</NavLink>
       </div>
       <div className={css.navItem}>
-        <a href='/music'>Music</a>
+        <NavLink to='/music' activeClassName={css.activeLink}>Music</NavLink>
       </div>
       <div className={css.navItem}>
-        <a href='/settings'>Settings</a>
+        <NavLink to='/settings' activeClassName={css.activeLink}>Settings</NavLink>
       </div>
     </nav>
   )
